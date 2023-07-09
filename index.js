bd7be9d95641b51c08bbf3a1541b60dc843cef15
SmithaEregowda <smithahe@gmail.com>
2023-07-09 11:32:58 +0530

    database changes

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const ErrorHandler=require('./middleware/errorHandler')
 const multer=require('multer');
 const path=require('path');
 const cors=require('cors')
+
 //to parse incoming req
  app.use(BodyParser.json())
 
@@ -85,7 +86,9 @@ app.use(ErrorHandler)
 //connecting data base
 const connectDatabase=async()=>{
    try{
-    const result = await mongoose.connect('mongodb+srv://smitha:smitha123@cluster0.pmlfv.mongodb.net/vegetableShop?retryWrites=true&w=majority');
+    const result = await mongoose.connect(
+      `mongodb+srv://smitha:smitha123@cluster0.pmlfv.mongodb.net/vegetableShop?retryWrites=true&w=majority`
+      );
     if(!result){
         const error=new Error('Failed to connect Database');
         throw error;
